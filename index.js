7d8ac89649d204f5a1b44b8505a3a04461a4c4c9
quarc0o <emmanuelq150@gmail.com>
2024-03-23 16:08:32 +0100

    gltf endpoint

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const fs = require("fs").promises;
+const port = process.env.PORT || 3000;
+
+function modifyGltf(gltfJson, newTextureUrl) {
+  gltfJson.images[0].uri = newTextureUrl;
+  return gltfJson;
+}
+
+app.get("/gltf", async (req, res) => {
+  const newTextureUrl = req.query.textureUrl;
+  if (!newTextureUrl) {
+    return res.status(400).send("Texture URL is required");
+  }
+
+  try {
+    const data = await fs.readFile("./assets/card.gltf", "utf8");
+    const gltfJson = JSON.parse(data);
+
+    console.log("Gltf file: ", gltfJson);
+
+    const modifiedGltfJson = modifyGltf(gltfJson, newTextureUrl);
+
+    res.json(modifiedGltfJson);
+  } catch (error) {
+    console.error("Error reading or modifying GLTF file:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
